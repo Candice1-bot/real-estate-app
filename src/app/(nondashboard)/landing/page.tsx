457bb1cadb48b22c19fda2b1c199794c2af0c1d8
Candice <candicejiang1@gmail.com>
2025-06-20 11:56:51 +0100

    initial

---
+++ src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeroSection />
+
+      <FeaturesSection />
+      <DiscoverSection />
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <div className="text-6xl bg-primary-100 p-10">Hello, I am here!</div>;
+}
